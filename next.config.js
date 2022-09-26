a4413f49fc536edc4557b51dff1223ee2ff9f801
Anqi Chen <anqi.chen@g42.ai>
2022-09-26 19:23:34 +0400

    v0.0.4 update, updated sonelist, changed next.config.js arrangement

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa')({
+  dest: 'public'
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
 
 module.exports = withPWA({
   trailingSlash: true,
@@ -9,8 +16,5 @@ module.exports = withPWA({
   },
   images: {
     loader: "custom"
-  },
-  pwa: {
-    dest: 'public'
   }
-});
+})
